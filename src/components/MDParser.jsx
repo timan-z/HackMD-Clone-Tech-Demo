e0430b2059ab75bfffb5c1b858e242657f913ff7
timan-z <58091424+timan-z@users.noreply.github.com>
2025-02-25 00:03:15 -0500

    my bad. markdown-it does recognize the quote function. I installed markdown-it-task-lists (external task list syntax) and use it with my parser function and now the checkbox works -- now when I have the parsedown format, it'll animate as a box. And when there's an X within the brackets, it'll animate as a box with the checkmark. Now, and this is to be expected, I can't click the box in the parser panel and have it pop a checkmark -- that's definitely something that I need to come to myself later...

--- src/components/MDParser.jsx
+++ src/components/MDParser.jsx
@@ -1,11 +1,12 @@
 
 import MarkdownIt from "markdown-it";
+import markdownItTaskLists from "markdown-it-task-lists";
 
 const md = new MarkdownIt({
     html: true,
     linkify: true,
     typographer: true,
-});
+}).use(markdownItTaskLists);
 
 // This function below is supposed to take Markdown text and convert it to HTML:
 export const parseMarkdown = (markdownText) => {
