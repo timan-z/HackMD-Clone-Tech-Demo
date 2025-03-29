dd327d8f008955a3ea8cdc83f8e8fe7aa773a174
timan-z <58091424+timan-z@users.noreply.github.com>
2025-03-28 22:07:11 -0400

    reverted some changes ahh if i screwed something up i will end it all

--- src/components/Socket.js
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io("http://localhost:4000"); // NOTE: This is what I'm picking for server port location in Server.js (maybe change it, doesn't matter, who cares).
-
-export default socket;
