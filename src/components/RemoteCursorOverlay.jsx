3e648ccca2fb9374582266bf37fff539f1fa1785
timan-z <58091424+timan-z@users.noreply.github.com>
2025-04-09 19:04:46 -0400

    have a perfectly working remote cursor stand-in (DecoratorNode) that looks exactly as I want it to look!

--- src/components/RemoteCursorOverlay.jsx
+++ src/components/RemoteCursorOverlay.jsx
@@ -1,5 +1,5 @@
 // This will be the overlay over the Text Editor contentEditable where the cursor markers will be placed (dynamically):
-import {useEffect, useRef} from "react";
+import React, {useEffect, useRef} from "react";
 
 export function RemoteCursorOverlay({editor, otherCursors, fontSize}) {
     const overlayRef = useRef(null);
@@ -22,7 +22,32 @@ export function RemoteCursorOverlay({editor, otherCursors, fontSize}) {
                 zIndex: 1,
             }}
         >
-            {/* Insert more code later... */}
+            {/* Static Dummy Cursor: */}
+            <div
+                style={{
+                    position: "absolute",
+                    left:"40px",
+                    top:"17px",
+                    width:"2px",
+                    height:"1.1em",
+                    backgroundColor:"red",
+                }}
+            > {/* Floating label next to cursor (for the ID): */}
+                <div
+                    style={{
+                        position:"absolute",
+                        top:"-1.5em",
+                        left:"3px",
+                        backgroundColor:"yellow",
+                        fontSize:"10px",
+                        padding:"2px 4px",
+                        borderRadius:"4px",
+                        whiteSpace:"nowrap",
+                    }}
+                >
+                    dummy-user-id
+                </div>
+            </div>
         </div>
     );
 }
